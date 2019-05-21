8cc7ac5d0b266be2bed091ebb4e0a9a513ed6fdc
Raphael Muenster <raphael.muenster@gmail.com>
2019-05-21 14:22:06 +0200

    Updated app

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ var config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }

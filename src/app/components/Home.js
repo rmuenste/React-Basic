0e155ad8900910c134f371ea951087187fe58258
Raphael Muenster <raphael.muenster@gmail.com>
2019-05-22 13:36:55 +0200

    Examples of component communication

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,49 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            powerLevel: props.powerLevel,
+            homeLink: "" 
+        };
+    }
+
+    onIncreasePower() {
+    // Careful the <this> keyword refers to the caller of the function,
+    // which may be a button in case of an event. So we have to make sure
+    // that <this> always refers to what it should by binding it correctly.
+        this.setState(prevState => ({
+            powerLevel: prevState.powerLevel + 3
+        }));
+    }
+
+    onHandleChange(ev) {
+        console.log("onHandleChange " + ev.target.value);
+        this.setState({
+            homeLink: ev.target.value
+        })
+
+    }
+
+    onChangeLink() {
+        console.log("onChangeLink  ");
+        this.props.changeLink(this.state.homeLink);
+    }
+
     render() {
-      return (
-          <div>
-              <p>In a new Component!</p>
-          </div>
-      )
+        return (
+            <div>
+                <p>In a new Component!</p>
+                <p>Your name is {this.props.name}, your power level is {this.state.powerLevel}</p>
+                <hr/>
+                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <button onClick={() => this.onIncreasePower()} className="btn btn-primary">Power++</button>
+                <hr/>
+                <input type="text" value={this.state.homeLink} onChange={(event) => this.onHandleChange(event)}/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
+            </div>
+        )
     };
 }
--- src/app/index.js
+++ src/app/index.js
@@ -4,17 +4,40 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            homeLink: "Home"
+        }
+    }
+
+    onGreet() {
+        alert("Hello!");
+    }
+
+    onChangeLinkName(newName) {
+        console.log("OnChangeLinkName: " + newName)
+        this.setState({
+            homeLink: newName
+        });
+    }
+    
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                      <Header/>
+                      <Header homeLink={this.state.homeLink}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                      <Home/>
+                      <Home name="Raphael" 
+                            powerLevel={10} 
+                            greet={this.onGreet} 
+                            changeLink={this.onChangeLinkName.bind(this)}
+                            initialLinkName={this.state.homeLink}
+                            />
                     </div>
                 </div>
             </div>

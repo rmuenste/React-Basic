c97ada87fe57048d1bc290f207edaa719d352841
Raphael Muenster <raphael.muenster@gmail.com>
2019-05-22 14:23:33 +0200

    changes for router example

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-export const Header = (props) =>  {
-    return(
-        <nav className="navbar navbar-default">
-            <div className="container">
-                <div className="navbar-header">
-                    <ul className="nav navbar-nav">
-                    <li>
-                        <a href="#">{props.homeLink}</a>
-                    </li>
-                    </ul>
+export class Header extends React.Component {
+    render() {
+        return(
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                        <ul className="nav navbar-nav">
+                        <li>
+                            <a href="#">Home</a>
+                            <a href="#">User</a>
+                        </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    );
+            </nav>
+
+        );
+    }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,46 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
-import { Header } from "./components/Header";
+
+import { Root } from "./components/Root";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {
-            homeLink: "Home"
-        }
-    }
-
-    onGreet() {
-        alert("Hello!");
-    }
-
-    onChangeLinkName(newName) {
-        console.log("OnChangeLinkName: " + newName)
-        this.setState({
-            homeLink: newName
-        });
     }
     
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                      <Header homeLink={this.state.homeLink}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                      <Home name="Raphael" 
-                            powerLevel={10} 
-                            greet={this.onGreet} 
-                            changeLink={this.onChangeLinkName.bind(this)}
-                            initialLinkName={this.state.homeLink}
-                            />
-                    </div>
-                </div>
-            </div>
+            <Root>
+                <Home></Home>
+            </Root>
         );
     }
 }
